2eece39e8b94f3431bdd969da2406a32c4531409
KartikNP31 <patelkn1303@gmail.com>
2024-04-28 19:04:14 +0530

    updates api.js

--- fitwell-reactjs/backend/routes/trainers.js
+++ fitwell-reactjs/backend/routes/trainers.js
@@ -1,12 +1,12 @@
 const express=require('express')
 const router=express.Router()
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+
 const TrainerService = require('../services/TrainerService')
 require('dotenv').config()
 
 
 router.post('/createTrainer', async(req, res)=>{
+    console.log("backend : welcome", req.body);
     const response=await TrainerService.createTrainer(req.body);
     return res.send(response); 
 })
--- fitwell-reactjs/frontend/src/api.js
+++ fitwell-reactjs/frontend/src/api.js
@@ -1,1 +1,2 @@
-export const PUBLIC_SERVER_URL="https://fitwell-reactjs.onrender.com"
+// export const PUBLIC_SERVER_URL="https://fitwell-reactjs.onrender.com"
+export const PUBLIC_SERVER_URL="http://localhost:5001"
